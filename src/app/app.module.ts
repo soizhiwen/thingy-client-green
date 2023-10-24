c8722f85749db1eae6b84e698417c3e6abc63167
3lava3 <liva.schaffner@bluewin.ch>
2023-10-24 17:44:41 +0200

    basic implementation of dashboard UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- tests/initial-test.spec.ts
+++ tests/initial-test.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:4200/');
 
-  await expect(page.getByRole('heading', { name: 'Thingy Client' })).toBeVisible();
+  await expect(page.getByText('Dashboard')).toBeVisible();
 });
