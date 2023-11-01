6010eca41ed21939a48ec34892c15073a6a1dd1c
3lava3 <liva.schaffner@bluewin.ch>
2023-11-01 09:33:20 +0100

    done with basic frontend that displays current data TG-7 #closed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { StoreModule } from '@ngrx/store';
+import { greenhouseReducer } from './state/greenhouse.reducer';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({ greenhouse: greenhouseReducer }, {}),
   ],
   providers: [],
   bootstrap: [AppComponent]
