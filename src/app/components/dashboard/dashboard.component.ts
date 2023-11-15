0af46e98805bc2bd654c347252e5a833cd421de0
3lava3 <liva.schaffner@bluewin.ch>
2023-11-15 11:22:40 +0100

    add current plants to dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { Store } from '@ngrx/store';
 import { selectCurrentAirQuality, selectCurrentCo2, selectCurrentHumidity, selectCurrentTemperature } from '../../state/greenhouse/greenhouse.selectors';
 import { Observable } from 'rxjs';
 import { DashboardActions } from '../../state/actions';
+import { Plant } from 'src/app/state/plant/plant.model';
+import { selectPlants } from 'src/app/state/plant/plant.selectors';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,14 +13,24 @@ import { DashboardActions } from '../../state/actions';
 })
 export class DashboardComponent implements OnInit {
 
-  currentTemperature$: Observable<number> = this.store.select(selectCurrentTemperature);
-  currentHumidity$: Observable<number> = this.store.select(selectCurrentHumidity);
-  currentCo2$: Observable<number> = this.store.select(selectCurrentCo2);
-  currentAirQuality$: Observable<number> = this.store.select(selectCurrentAirQuality);
+  currentDetails = [
+    { name: 'Temperature', value$: this.store.select(selectCurrentTemperature), img: "../../assets/temperature.png", unit: '°C' },
+    { name: 'Humidity', value$: this.store.select(selectCurrentHumidity), img: "../../assets/humidity.png", unit: '%' },
+    { name: 'CO2 Level', value$: this.store.select(selectCurrentCo2), img: "../../assets/co2.png" },
+    { name: 'Air Quality', value$: this.store.select(selectCurrentAirQuality), img: "../../assets/air-quality.png" }
+  ]
+
+  currentPlants$: Observable<Plant[]> = this.store.select(selectPlants);
+
+  plantSelected = false;
 
   constructor(private store: Store) { }
 
   ngOnInit(): void {
     this.store.dispatch(DashboardActions.loadGreenhouseData())
+  }
+
+  daysLeft(date: Date): number {
+    return Math.ceil((date.getTime() - Date.now()) / (1000 * 3600 * 24));
   }
 }
